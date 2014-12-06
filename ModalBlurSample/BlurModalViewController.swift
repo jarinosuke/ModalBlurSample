74403faf1dd76009b1a59d192676574a75cd71c9
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2014-12-06 13:27:17 +0900

    Added empty modal blur view controller.

---
+++ ModalBlurSample/BlurModalViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BlurModalViewController.swift
+//  ModalBlurSample
+//
+//  Created by Naoki Ishikawa on 12/6/14.
+//  Copyright (c) 2014 Naoki Ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class BlurModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
